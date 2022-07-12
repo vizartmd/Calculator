414bc1229103ca417dce632c240d05839a0f3fe8
Robu Victor <vizart.md@gmail.com>
2022-07-12 13:15:47 +0300

    rezolved conglicts in Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,32 +1,9 @@
 package sample;
 
-<<<<<<< HEAD
 public class Main {
     public static void main(String[] args) {
         MainWindow mainWindow = new MainWindow();
         mainWindow.run();
-=======
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
->>>>>>> 43d1732115e47b0342146402ebe70d22c6fd235c
     }
 }
+

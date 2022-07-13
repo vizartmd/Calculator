c62ef7ab10c75ce34787b829669b6594e68f6113
Robu Victor <vizart.md@gmail.com>
2022-07-13 12:43:43 +0300

    ListView has been added to display the operation history

--- src/sample/MainWindowController.java
+++ src/sample/MainWindowController.java
@@ -4,12 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class MainWindowController {
@@ -18,12 +21,13 @@ public class MainWindowController {
     @FXML private ImageView btnMinimize, btnMaximize, btnClose;
     @FXML private Label lblResult;
     @FXML private Label lblInput;
+    @FXML private ListView<String> myListView;
 
     private double x, y;
     private double num1 = 0;
     private double num2;
     private String operator = "+";
-    private Queue<String> resultQueue;
+    private final List<String> resultQueue = new LinkedList<>();
 
     public void init(Stage stage) {
         titlePane.setOnMousePressed(mouseEvent -> {
@@ -45,7 +49,7 @@ public class MainWindowController {
     @FXML
     void onNumericClicked(MouseEvent event) {
         int value = Integer.parseInt(((Button)event.getSource()).getId().replace("btn", ""));
-        lblInput.setText(String.valueOf(Math.round(Float.parseFloat(Double.parseDouble(lblInput.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(lblInput.getText())*10+value)))));
+        lblInput.setText(Double.parseDouble(lblInput.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(lblInput.getText())*10+value));
     }
 
     @FXML
@@ -54,22 +58,52 @@ public class MainWindowController {
         if (symbol.equals("Equals")) {
             num2 = Double.parseDouble(lblInput.getText());
             switch (operator) {
-                case "+" -> lblResult.setText((num1+num2) + "");
-                case "-" -> lblResult.setText((num1-num2) + "");
-                case "*" -> lblResult.setText((num1*num2) + "");
-                case "/" -> lblResult.setText((num1/num2) + "");
+                case "+" -> {
+                    resultQueue.add((num1+num2) + "");
+//                    lblResult.setText(resultQueue.toString());
+                    lblInput.setText(String.valueOf(0));
+                    myListView.getItems().add((num1+num2) + "");
+                }
+                case "-" -> {
+                    resultQueue.add((num1-num2) + "");
+//                    lblResult.setText(resultQueue.toString());
+                    lblInput.setText(String.valueOf(0));
+                    myListView.getItems().removeAll();
+                    myListView.getItems().add((num1-num2) + "");
+                }
+                case "*" -> {
+                    resultQueue.add((num1*num2) + "");
+//                    lblResult.setText(resultQueue.toString());
+                    lblInput.setText(String.valueOf(0));
+                    myListView.getItems().removeAll();
+                    myListView.getItems().add((num1*num2) + "");
+                }
+                case "/" -> {
+                    resultQueue.add((num1/num2) + "");
+//                    lblResult.setText(resultQueue.toString());
+                    lblInput.setText(String.valueOf(0));
+                    myListView.getItems().removeAll();
+                    myListView.getItems().add((num1/num2) + "");
+                }
             }
-            resetResult();
         }
         else if (symbol.equals("Clear")) {
             resetResult();
         }
         else {
             switch (symbol) {
-                case "Plus" -> operator = "+";
-                case "Minus" -> operator = "-";
-                case "Multiply" -> operator = "*";
-                case "Divide" -> operator = "/";
+                case "Plus" -> {
+                    operator = "+";
+                }
+                case "Minus" -> {
+                    operator = "-";
+                }
+                case "Multiply" -> {
+                    operator = "*";
+                }
+                case "Divide" -> {
+                    operator = "/";
+                }
             }
             num1 = Double.parseDouble(lblInput.getText());
             lblInput.setText(String.valueOf(0));

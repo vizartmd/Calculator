016b2dd7eb8c1de43a97ee9e01426e6031204dd1
Robu Victor <vizart.md@gmail.com>
2022-07-14 15:53:46 +0300

    commit before first review, the first stage completed, only mouse click data entry is implemented

--- src/sample/MainWindowController.java
+++ src/sample/MainWindowController.java
@@ -11,6 +11,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -19,14 +20,15 @@ public class MainWindowController {
 
     @FXML private Pane titlePane;
     @FXML private ImageView btnMinimize, btnMaximize, btnClose;
-    @FXML private Label lblResult;
     @FXML private Label lblInput;
+    @FXML private Label lblInput1;
     @FXML private ListView<String> myListView;
 
     private double x, y;
     private double num1 = 0;
     private double num2;
     private String operator = "+";
+    private String inputProcess = "";
     private final List<String> resultQueue = new LinkedList<>();
 
 
@@ -36,8 +38,8 @@ public class MainWindowController {
             y = mouseEvent.getSceneY();
         });
         titlePane.setOnMouseDragged(mouseEvent -> {
-            stage.setX(mouseEvent.getSceneX()-x);
-            stage.setY(mouseEvent.getSceneY()-y);
+            stage.getScene().getWindow().setX(mouseEvent.getSceneX()-x);
+            stage.getScene().getWindow().setY(mouseEvent.getSceneY()-y);
         });
 
         btnClose.setOnMouseClicked(mouseEvent -> stage.close());
@@ -46,78 +48,130 @@ public class MainWindowController {
             stage.setFullScreen(!stage.isFullScreen());
         });
         myListView.setEditable(false);
+        lblInput1.setText(inputProcess);
     }
 
     @FXML
     void onNumericClicked(MouseEvent event) {
         int value = Integer.parseInt(((Button)event.getSource()).getId().replace("btn", ""));
-        lblInput.setText(Double.parseDouble(lblInput.getText())==0?String.valueOf((double)value):String.valueOf(Double.parseDouble(lblInput.getText())*10+value));
+        lblInput.setText(inputProcess.equals("")?String.valueOf(value):String.valueOf(Math.round(Integer.parseInt(lblInput.getText())*10+value)));
+        inputProcess += value;
+        lblInput1.setText(inputProcess);
     }
 
     @FXML
     void onSymbolClicked(MouseEvent event) {
         String symbol = ((Button)event.getSource()).getId().replace("btn", "");
         if (symbol.equals("Equals")) {
-            num2 = Double.parseDouble(lblInput.getText());
+            num2 = Integer.parseInt(lblInput.getText());
             switch (operator) {
                 case "+" -> {
                     resultQueue.add((num1+num2) + "");
-//                    lblResult.setText(resultQueue.toString());
-                    lblInput.setText(String.valueOf(0));
-                    myListView.getItems().add(0,(num1+num2) + "");
+                    lblInput.setText(String.valueOf(num1+num2));
+                    if (needToRound((num1+num2))) {
+                        myListView.getItems().add(0, inputProcess + " = " + ((int)(num1+num2)) + "");
+                        lblInput1.setText(String.valueOf(((int)(num1+num2))));
+                    } else {
+                        myListView.getItems().add(0, inputProcess + " = " + (num1+num2) + "");
+                        lblInput1.setText(String.valueOf(num1+num2));
+                    }
                     resetResult();
                 }
                 case "-" -> {
                     resultQueue.add((num1-num2) + "");
-//                    lblResult.setText(resultQueue.toString());
-                    lblInput.setText(String.valueOf(0));
-                    myListView.getItems().removeAll();
-                    myListView.getItems().add(0,(num1-num2) + "");
+                    lblInput.setText(String.valueOf((num1-num2)));
+                    if (needToRound((num1-num2))) {
+                        myListView.getItems().add(0, inputProcess + " = " + ((int)(num1-num2)) + "");
+                        lblInput1.setText(String.valueOf(((int)(num1-num2))));
+                    } else {
+                        myListView.getItems().add(0, inputProcess + " = " + (num1-num2) + "");
+                        lblInput1.setText(String.valueOf(num1-num2));
+                    }
                     resetResult();
                 }
                 case "*" -> {
                     resultQueue.add((num1*num2) + "");
-//                    lblResult.setText(resultQueue.toString());
-                    lblInput.setText(String.valueOf(0));
-                    myListView.getItems().removeAll();
-                    myListView.getItems().add(0, (num1*num2) + "");
+                    lblInput.setText(String.valueOf(num1*num2));
+                    if (needToRound((num1*num2))) {
+                        myListView.getItems().add(0, inputProcess + " = " + ((int)(num1*num2)) + "");
+                        lblInput1.setText(String.valueOf(((int)(num1*num2))));
+                    } else {
+                        myListView.getItems().add(0, inputProcess + " = " + (num1*num2) + "");
+                        lblInput1.setText(String.valueOf(num1*num2));
+                    }
                     resetResult();
                 }
                 case "/" -> {
                     resultQueue.add((num1/num2) + "");
-//                    lblResult.setText(resultQueue.toString());
-                    lblInput.setText(String.valueOf(0));
-                    myListView.getItems().removeAll();
-                    myListView.getItems().add(0, (num1/num2) + "");
+                    lblInput.setText(String.valueOf(num1/num2));
+                    if (needToRound((num1/num2))) {
+                        myListView.getItems().add(0, inputProcess + " = " + ((int)(num1/num2)) + "");
+                        lblInput1.setText(String.valueOf(((int)(num1/num2))));
+                    } else {
+                        myListView.getItems().add(0, inputProcess + " = " + (num1/num2) + "");
+                        lblInput1.setText(String.valueOf(num1/num2));
+                    }
                     resetResult();
                 }
             }
         }
         else if (symbol.equals("Clear")) {
-            resetResult();
+            reset();
         }
         else {
             switch (symbol) {
                 case "Plus" -> {
                     operator = "+";
+                    inputProcess += (" " + operator + " ");
                 }
                 case "Minus" -> {
                     operator = "-";
+                    inputProcess += (" " + operator + " ");
                 }
                 case "Multiply" -> {
                     operator = "*";
+                    inputProcess += (" " + operator + " ");
                 }
                 case "Divide" -> {
                     operator = "/";
+                    inputProcess += (" " + operator + " ");
                 }
             }
-            num1 = Double.parseDouble(lblInput.getText());
+            num1 = Integer.parseInt(lblInput.getText());
             lblInput.setText(String.valueOf(0));
+            lblInput1.setText(inputProcess);
         }
+    }
+
+    private boolean needToRound(double num) {
+        String numToString = String.valueOf(num);
+        int decimalIndex = numToString.indexOf(".");
+        numToString = numToString.substring(decimalIndex+1);
+        char[] arr = numToString.toCharArray();
+        boolean need = true;
+        for (char c : arr) {
+            if (c != '0') {
+                need = false;
+                break;
+            }
+        }
+        return need;
     }
 
     private void resetResult() {
         lblInput.setText(String.valueOf(0));
         operator = ".";
+        inputProcess = "";
+    }
+
+    private void reset() {
+        lblInput1.setText(String.valueOf(0));
+        operator = ".";
+        inputProcess = "";
+    }
+
+    @FXML
+    private void key (KeyEvent evt) {
+        System.out.println("evt.getCode() = " + evt.getCode());
     }
 }

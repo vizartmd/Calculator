4e117622b6a6a7d6b9d10696fbf5747f151caa37
vizartmd <vizart.md@gmail.com>
2022-07-23 15:04:40 +0300

    added two buttons on title pane

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,16 +1,20 @@
 package org.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PopupControl;
 import javafx.scene.paint.Color;
+import javafx.stage.Popup;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 /**
  * JavaFX App
  */
-public class  App extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
@@ -24,7 +28,6 @@ public class  App extends Application {
         ((MainWindowController)loader.getController()).init(stage);
         stage.show();
     }
-
 
     public void run() {
         launch();
